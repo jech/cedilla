cce59bf498df179366195fe9f87298e477a96b67
Juliusz Chroboczek <jch@pps.jussieu.fr>
2006-02-15 21:45:06 +0100

    Added alternative glyphs for most combining characters.

--- data-add.lisp
+++ data-add.lisp
@@ -1,5 +1,5 @@
 ;;; This file is part of Cedilla.
-;;; Copyright (C) 2002 by Juliusz Chroboczek.
+;;; Copyright (C) 2002-2006 by Juliusz Chroboczek.
 
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -21,97 +21,119 @@
   (setup-cedilla-readtable))
 
 (define-alternatives
-  ;; Combining characters; only a few are computed from the UnicodeData file
-  (#u300 #u60)
-  (#u0302 #u2C6)                        ; "circumflex"
-  (#u0302 #u5E)                         ; "asciicircum"
+  (#u00AD #\-)
+  (#u0110 #uD0)
+  (#u0110 (:bar . #\D))
+  (#u0111 (:bar . #\d))
+  (#u0126 (:bar . #\H))
+  (#u0127 (:bar . #\h))
+  (#u0166 (:bar . #\T))
+  (#u0167 (:bar . #\t))
+  (#u0197 (:bar . #\I))
+  (#u01B5 (:bar . #\Z))
+  (#u01B6 (:bar . #\z))
+  (#u01E4 (:bar . #\G))
+  (#u01E5 (:bar . #\g))
+  (#u0268 (:bar . #\i))
+  (#u0300 #u60)
+  (#u0302 #u5E)
+  (#u0302 #u2C6)
   (#u0304 #\-)
-  (#u0305 #u2014)                       ; after "overscore"
-  (#u0305 #uAF)
   (#u0305 #\-)
-  (#u030B (:double-h . #u301))          ; after "hungarumlaut"
+  (#u0305 #u00AF)
+  (#u0305 #u2014)
+  ;; Don't include this as it would cause the dot of a dotted i to be
+  ;; removed when composing.
+  ;;  ((#u0307 . #\i) #\i)
+  (#u030B (:double-h . #u301))
   (#u030C #u2C7)
-  (#u030D #\')                          ; vertical line above
-  (#u030E #\")                          ; double vertical line above
-  (#u030F (:double-h . #u300))          ; double grave accent
-  (#u0310 (#u30D . #u306))
+  (#u030D #\')
+  (#u030E #\")
+  (#u030F (:double-h . #u300))
+  (#u0310 (:simplify . (#u307 . #u306)))
   (#u0311 (:mirror-v . #u306))
-  (#u0312 #u2018)                       ; turned comma above
+  (#u0312 #u2018)
   (#u0313 #\,)
   (#u0313 #u1FBF)
-  (#u0314 (:mirror-h . #u313))              ; reversed comma above
+  (#u0314 (:mirror-h . #u313))
   (#u0315 #\,)
   (#u0316 #\`)
   (#u0317 #uB4)
-  ;; ...
-  (#u00AD #\-)
+  (#u0318 (:small . #u22A3))
+  (#u0319 (:small . #u22A2))
+  (#u031D (:small . #u22A5))
+  (#u031E (:small . #u22A4))
   (#u031F (:small . #\+))
   (#u0320 (:small . #\-))
   (#u0323 #u2D9)
   (#u0324 #uA8)
   (#u0325 #u2DA)
   (#u0326 #\,)
-  (#u0329 #\')                          ; vertical line below
+  (#u0329 #\')
   (#u032C #u2C7)
   (#u032D #u2C6)
   (#u032E #u2D8)
   (#u032F #u311)
   (#u0330 #u2DC)
-  (#u0331 #uAF)
   (#u0331 #\-)
+  (#u0331 #uAF)
+  (#u0332 #\-)
   (#u0332 #u2014)
   (#u0332 #uAF)
-  (#u0332 #\-)
-  (#u0333 (:double-v . #u305))          ; double low line below
+  (#u0333 (:double-v . #u305))
   (#u0334 #u2DC)
   (#u0335 #\-)
   (#u0336 #u2014)
   (#u0336 #uAF)
   (#u0337 #\/)
-  (#u0338 #u2044)
   (#u0338 #\/)
+  (#u0338 #u2044)
+  (#u033B (:small . #u25A1))
   (#u033D (:small . #uD7))
   (#u033E (:rotate . #\~))
-  (#u033F (:double-v . #u305))          ; double overline
-  (#u0342 #u2DC)                        ; perispomeni
+  (#u033F (:double-v . #u305))
+  (#u0342 #u2DC)
   (#u0345 (:small . #u3B9))
   (#u0347 (:small . #\=))
-  (#u0348 #\")                          ; double veritical line below
+  (#u0348 #\")
+  (#u034A (:small . (:simplify . #u2241)))
+  (#u034B (:small . (:simplify . #u223B)))
+  (#u034C (:small . #u2248))
+  (#u034D (:small . (:simplify . #u2194)))
+  (#u034E (:small . #u2191))
   (#u0360 (:double-v . #u303))
   (#u0361 (:double-v . #u311))
+  (#u0362 (:small . #u21C9))
+  (#u0363 (:small . #\a))
+  (#u0364 (:small . #\e))
+  (#u0365 (:small . #\i))
+  (#u0366 (:small . #\o))
+  (#u0367 (:small . #\u))
+  (#u0368 (:small . #\c))
+  (#u0369 (:small . #\d))
+  (#u036A (:small . #\h))
+  (#u036B (:small . #\m))
+  (#u036C (:small . #\r))
+  (#u036D (:small . #\t))
+  (#u036E (:small . #\v))
+  (#u036F (:small . #\x))
+  (#u1FFE #u2018)
+  (#u200C #u200B)
+  (#u200D #u200B)
   (#u20D2 #\|)
   (#u20D3 (:small . #\|))
-  (#u20D8 #u2DA)
   (#u20D6 #u2190)
   (#u20D7 #u2192)
+  (#u20D8 #u2DA)
   (#u20DB (:small . #u2234))
   (#u20DC (:small . "::"))
-  (#u1FFE #u2018)                       ; dasia
-  ;; random other stuff
-  (#u110 #uD0)                          ; D-bar -> Eth
-  ;; Don't include this as it would cause the dot of a dotted i to be
-  ;; removed when composing.
-  ;;  ((#u307 . #\i) #\i)
-  (#u200C #u200B)                       ; zwj
-  (#u200D #u200B)                       ; zwnj 
-  (#u2218 #u25CB)
-  ;; magic
-  (#u110 (:bar . #\D))
-  (#u111 (:bar . #\d))
-  (#u1E4 (:bar . #\G))
-  (#u1E5 (:bar . #\g))
-  (#u126 (:bar . #\H))
-  (#u127 (:bar . #\h))
-  (#u197 (:bar . #\I))
-  (#u268 (:bar . #\i))
-  (#u166 (:bar . #\T))
-  (#u167 (:bar . #\t))
-  (#u1B5 (:bar . #\Z))
-  (#u1B6 (:bar . #\z))
   (#u2127 (:mirror-v . #u2126))
   (#u2129 (:turn . #u3B9))
   (#u2132 (:turn . #\F))
   (#u213A (:rotate . #\Q))
+  (#u21C7 (:double-v . #u2190))
+  (#u21C9 (:double-v . #u2192))
+  (#u2218 #u25CB)
   (#u2295 (:small-circle . #\+))
   (#u2296 (:small-circle . #\-))
   (#u2297 (:small-circle . #uD7))
@@ -125,19 +147,17 @@
   (#u229F (:square . #\-))
   (#u22A0 (:square . #uD7))
   (#u22A1 (:square . #uB7))
+  (#u22A2 (:rotate-right . #u22A5))
+  (#u22A3 (:rotate . #u22A5))
+  (#u22A4 (:turn . #u22A5))
 )
 
 (define-fallbacks
-  ;; Combining characters
-  (#u20D0 #u20D6)
-  (#u20D1 #u20D7)
-  ;; Non-combining characters
   (#u0138 (:lowercase . #\K))
   (#u018F (:stretch . #u0259))
   (#u01DD (:turn . #\e))
   (#u0258 (:mirror-h . #\e))
   (#u0259 (:turn . #\e))
-  ;; Modifier letters
   (#u02B4 (:super . (:turn . #\r)))
   (#u02B6 (:super . (:mirror-v . #\R)))
   (#u02B9 #\')
@@ -156,36 +176,36 @@
   (#u02CE (#u316 . #\Space))
   (#u02CF (#u317 . #\Space))
   (#u02EE #.(coerce '(#u2BC #u2BC) 'vector))
-  ;; Punctuation
-  (#u2010 #\-)                          ; hyphen
-  (#u2011 #\-)                          ; non-breaking hyphen
-  (#u2012 #\-)                          ; figure dash
-  (#u2013 #\-)                          ; en dash
-  (#u2014 #\-)                          ; em dash
-  (#u2015 #u2014)                       ; horizontal bar
+  (#u2010 #\-)
+  (#u2011 #\-)
+  (#u2012 #\-)
+  (#u2013 #\-)
+  (#u2014 #\-)
+  (#u2015 #u2014)
   (#u2016 (:double-h . #\|))
   (#u201A #\,)
-  (#u201B #u2018)                       ; high-reversed-9 q. mark
+  (#u201B #u2018)
   (#u201E #.(coerce '(#u201A #u201A) 'string))
-  (#u201F #u201C)                       ; double high-reversed-9 q. mark
+  (#u201F #u201C)
   (#u2032 #\')
   (#u2035 #u2018)
   (#u2043 #\-)
   (#u2044 #\/)
+  (#u20D0 #u20D6)
+  (#u20D1 #u20D7)
   (#u212E #\e)
-  ;; Mathematical operators
   (#u220A (:small . #u2208))
   (#u220B (:mirror-h . #u2208))
   (#u220D (:small . #u220B))
   (#u2210 (:mirror-v . #u220F))
   (#u2212 #\-)
-  (#u2223 #\|)
-  (#u2225 #u2016)
   (#u2213 (:mirror-v . #u00B1))
   (#u2214 (#u0307 . #\+))
   (#u2216 #\\)
   (#u221B #.(coerce '(#u00B3 #u221A) 'vector))
   (#u221C #.(coerce '(#u2074 #u221A) 'vector))
+  (#u2223 #\|)
+  (#u2225 #u2016)
   (#u2225 #u2016)
   (#u2235 (:mirror-v . #u2234))
   (#u2236 #\:)
@@ -201,6 +221,10 @@
   (#u2267 (#u0347 . #\>))
   (#u226A (:double-h . #\<))
   (#u226B (:double-h . #\>))
+  (#u22A6 #u22A2)
+  (#u22A7 (:double-v . #u22A2))
+  (#u22A8 (:double-v . #u22A2))
+  (#u22A9 (:double-h . #u22A2))
   (#u22C4 #u25C7)
   (#u22C6 #u2217)
   )
@@ -211,10 +235,9 @@
   (#u0306 "cyrbreve")
   (#u0307 "dot")
   (#u030F "dblgrave")
-  (#u0311 "invbreve")     ; ???
-  ;; ...
-  (#u0332 "overscore")    ; low line below
+  (#u0311 "invbreve")
+  (#u0332 "overscore")
   (#u0336 "overscore")
-  (#u03BC "mu")
   (#u0394 "Delta")
+  (#u03BC "mu")
 )
